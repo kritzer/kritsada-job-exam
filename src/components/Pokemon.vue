7931b35979c2f1699ec61c7825fdf8bd7bf31fa6
Kritzer <kissyoukissme93@gmail.com>
2020-11-13 01:28:37 +0700

    แก้ไข Reonsive Function 1 และ 2

--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -1,75 +1,49 @@
 <template>
-  <v-app>
-      <v-card class="mx-auto" width="100%">
+<v-app>
+    <v-card class="mx-auto" width="100%">
         <v-card-title class="d-flex align-center justify-center mx-auto">
             Test fetch API
         </v-card-title>
         <v-row>
-            <v-col
-            v-for="(item, id) in pokemons"
-            :key="id"
-            class="d-flex child-flex"
-            cols="2"
-            >
-                <v-img
-                    :src="item.url"
-                    class="grey lighten-2"
-                    v-if="item.id !== ''"
-                >
-                    <template v-slot:placeholder>
-                            <v-row
-                                class="fill-height ma-0"
-                                align="center"
-                                justify="center"
-                            >
-                                <v-progress-circular
-                                indeterminate
-                                color="grey lighten-5"
-                                ></v-progress-circular>
-                            </v-row>
-                    </template>
+            <v-col v-for="(item, id) in pokemons" :key="id" class="d-flex child-flex" cols="2" max-width="100%">
+                <v-img lazy-src="https://picsum.photos/id/11/10/6" :src="item.url" class="grey lighten-2" v-if="item.id !== ''" max-height="70%" max-width="30%">
                 </v-img>
                 <v-subheader>
                     {{item.name}}
                 </v-subheader>
             </v-col>
         </v-row>
-      </v-card>
-  </v-app>
+    </v-card>
+</v-app>
 </template>
 
 <script>
 import Controller from '../services/Controller'
 export default {
-data:() =>({
-pokemons:[{
-    id:'',
-    name:'',
-    url:'',
-}],
-}),
-methods:{
-    getPoke:async function(){
-        for(let i = 0;i < 105;i++){
-            Controller.getPokemon(i+1).then(response=>{
-                if(response.data.id == i+1){
-                this.pokemons.push({
-                    id: response.data.id,
-                    name: response.data.name,
-                    url: response.data.sprites.front_default
-                })                      
-                }
-        })
-        }
+    data: () => ({
+        pokemons: [],
+    }),
+    methods: {
+        getPoke: function () {
 
+            for (let i = 1; i < 105; i++) {
+                Controller.getPokemon(i).then(response => {
+                    this.pokemons.push({
+                        no: response.data.id,
+                        name: response.data.name,
+                        url: response.data.sprites.front_default
+                    })
+                })
+            }
+
+        },
+    },
+    mounted() {
+
+    },
+    created() {
+        this.getPoke();
     }
-},
-mounted() {
-
-},
-created(){
-    this.getPoke();
-}
 }
 </script>
 
--- src/services/Controller.js
+++ src/services/Controller.js
@@ -4,12 +4,13 @@ const SERVER_URL = 'https://pokeapi.co/api/v2';
 
 const instance = axios.create({
     baseURL: SERVER_URL,
-    timeout: 15000
+    timeout: 3000
   });
+
   export default {
-      getPokemon: (id) => instance.get('/pokemon/'+id,{
+    getPokemon: (id) => instance.get('/pokemon/'+id,{
           transformResponse: (res) => {
-              return JSON.parse(res)
+              return  JSON.parse(res) ;
           }
-      })
+    })
   }
